8bbb21b0ee19070043905f6d288c015a1864b100
JuanCabre <johancabrerag@gmail.com>
2015-05-06 10:37:04 +0200

    Added suggestions from the discussion Signed-off-to-Steinwurf-by: Juan Cabrera jcabre14@student.aau.dk

--- examples/creating_printing_table/creating_printing_table.cpp
+++ examples/creating_printing_table/creating_printing_table.cpp
@@ -21,25 +21,30 @@
 int main()
 {
 
-    // In this example we will generate 100 random numbers. We then
-    // will store those numbers in a table with two columns:
-    // The random number and the iteration in wich it was generated.
+    // In this example we will generate 100 random numbers. We will
+    // then store those numbers in a table with three columns:
+    // The random number, the iteration in which it was generated and
+    // a string saying either 'high' or 'low'. The third column shows
+    // that different types of data can be stored in the table.  
 
     // We set the names for the ouput files.
     std::string out_csv = "out.csv";
     std::string out_json = "out.json";
-    std::string out_python = "out_python.json";
+    std::string out_python = "out.py";
 
     // We will make the table object where the
     // results are going to be stored.
     tables::table results;
 
-    // We first create the two columns of the table.
+    // We first create the three columns of the table.
     if (!results.has_column("Iteration"))
         results.add_column("Iteration");
 
     if (!results.has_column("Number"))
         results.add_column("Number");
+
+    if (!results.has_column("Classification"))
+        results.add_column("Classification");
 
     for (uint8_t i = 0; i < 100; ++i)
     {
@@ -49,17 +54,18 @@ int main()
         // Add a row to the table.
         results.add_row();
 
-        // Store the generated number and the iteration
-        // in the table.
+        // Store the generated number, the iteration
+        // and the string in the table.
         results.set_value("Iteration", i);
         results.set_value("Number", random_number);
+        results.set_value("Classification", random_number > RAND_MAX / 2 ? 
+            std::string("low") : std::string("high"));
     }
 
-
-    // We create the ofstream objects
-    std::ofstream ofc(out_csv);
-    std::ofstream ofj(out_json);
-    std::ofstream ofp(out_python);
+    // We create the ofstream objects.
+    std::ofstream csv_ofstream(out_csv);
+    std::ofstream json_ofstream(out_json);
+    std::ofstream python_ofstream(out_python);
 
     // We create the printers objects, each one will print
     // in an output file the table in a different format.
@@ -68,10 +74,9 @@ int main()
     tables::python_format printer_python;
 
     // We call the print methods
-    printer_csv.print(ofc, results);
-    printer_json.print(ofj, results);
-    printer_python.print(ofp, results);
+    printer_csv.print(csv_ofstream, results);
+    printer_json.print(json_ofstream, results);
+    printer_python.print(python_ofstream, results);
 
     return 0;
-
 }
